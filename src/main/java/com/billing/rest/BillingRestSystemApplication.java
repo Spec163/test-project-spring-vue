89d0ae7b956532fc714508dc368a07daeb42656e
Spec163 <45912087+Spec163@users.noreply.github.com>
2020-06-04 20:20:23 +0400

    initial commit

---
+++ src/main/java/com/billing/rest/BillingRestSystemApplication.java
@@ -0,0 +1,13 @@
+package com.billing.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingRestSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingRestSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/billing/rest/BillingRestSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billing.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingRestSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
